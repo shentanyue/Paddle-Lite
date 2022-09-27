cd16d1c8940c9d0eada5f26b3810b8ad72f7f778
root <root@localhost.localdomain>
2022-09-27 10:35:58 +0000

    dynamic range enhance test=huawei_ascend_npu

--- lite/backends/nnadapter/nnadapter/src/driver/huawei_ascend_npu/converter/lp_normalization.cc
+++ lite/backends/nnadapter/nnadapter/src/driver/huawei_ascend_npu/converter/lp_normalization.cc
@@ -32,12 +32,19 @@ int ConvertLpNormalization(Converter* converter, core::Operation* operation) {
   if (!input_operator) {
     input_operator = converter->ConvertOperand(input_operand);
   }
+#if NNADAPTER_HUAWEI_ASCEND_NPU_CANN_VERSION_GREATER_THAN(5, 0, 3)
+  auto norm_op = converter->AddOperator<ge::op::LpNorm>(output_operand);
+  norm_op->set_attr_axes(axis);
+  norm_op->set_attr_epsilon(epsilon);
+  norm_op->set_attr_p(p);
+#else
   NNADAPTER_CHECK_EQ(p, 2) << "HUAWEI_ASCEND_NPU only support p = 2";
-  auto l2_norm_op = converter->AddOperator<ge::op::L2Normalize>(output_operand);
-  l2_norm_op->set_attr_axis(axis);
-  l2_norm_op->set_attr_eps(epsilon);
-  SET_INPUT(l2_norm_op, x, input_operator);
-  MAP_OUTPUT(l2_norm_op, y, output_operand);
+  auto norm_op = converter->AddOperator<ge::op::L2Normalize>(output_operand);
+  norm_op->set_attr_axis(axis);
+  norm_op->set_attr_eps(epsilon);
+#endif
+  SET_INPUT(norm_op, x, input_operator);
+  MAP_OUTPUT(norm_op, y, output_operand);
   return NNADAPTER_NO_ERROR;
 }
 
